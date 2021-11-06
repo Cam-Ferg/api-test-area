a124c11fb7e645d5c050504d2e62ce2fed7aae0d
unknown <camburgerson@gmail.com>
2021-11-07 12:52:26 +1300

    set up name email and secret areas

---
+++ test-area/src/components/Progress.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Progress = () => {
+  return (
+    <React.Fragment>
+      <div className="steps">
+        <div className="step">
+         
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Progress;
